12e698a406870e673092b9aef1ed043616535a07
Ahmed Eid <aeid@hypermedialabs.com>
2022-09-01 03:01:41 +0200

    changed google drive link

--- component/Header/Header.jsx
+++ component/Header/Header.jsx
@@ -4,10 +4,20 @@ import Link from '../Link/Link';
 import SectionLink from '../SectionLink/SectionLink';
 import Span from '../Span/Span';
 import classes from './Header.module.scss';
+import AnimatedText from '../AnimatedText/AnimatedText';
+import { motion } from 'framer-motion';
+
+const container = {
+  visible: {
+    transition: {
+      staggerChildren: 0.025,
+    },
+  },
+};
 
 const Header = ({ aboutRef, projectsRef }) => {
   const onScrollHandler = (ref) => {
-    ref?.current?.scrollIntoView({ behavior: 'smooth',  });
+    ref?.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
@@ -21,7 +31,17 @@ const Header = ({ aboutRef, projectsRef }) => {
             Front / React End<Span color='green'> Developer </Span>
           </p>
           <Divider />
+          <motion.div
+            className='App'
+            initial='hidden'
+            // animate="visible"
+            animate='visible'
+            variants={container}
+          >
+            <AnimatedText text='hello world' tag='heading2' />
+          </motion.div>
         </div>
+
         <div className={classes.Header__SectionLinks}>
           <SectionLink
             onClick={() => {
@@ -51,7 +71,7 @@ const Header = ({ aboutRef, projectsRef }) => {
           />
           <Link
             icon='document-text-outline'
-            href='https://drive.google.com/file/d/1llEvVaj36Ap1zPTzUZ3wXB1t5j0BY0Z4/view?usp=sharing'
+            href='https://drive.google.com/file/d/1t4ORNkuZFUsgbXv5S4ks_w4TBfE8PRve/view?usp=sharing'
             name='Resume'
             newTab
           />
